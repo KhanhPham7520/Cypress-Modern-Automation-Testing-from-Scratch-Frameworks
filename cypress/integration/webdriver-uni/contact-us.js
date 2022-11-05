b35fd4ed3d750f4d4284c31d9fe1e9c13d023987
Pham Phan Nhat Khanh <khanh.pham@upmesh.io>
2022-11-06 00:30:49 +0700

    Complete Assertion

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -3,8 +3,14 @@
 describe("Test Contact US for via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
-       // cy.get('#contact-us').click({force : true})
-       // cy.get("input[placeholder='First Name']").type("Pham Phan")
+        cy.document().should("have.property","charset").and('eq','UTF-8')
+
+        //check title name of tab
+       cy.title().should('include','Contact Us')
+       
+       //check url was correct
+       cy.url().should('include','contactus')
+
        cy.get("[name='first_name']").type("Pham Phan")
        cy.get("[name='last_name']").type("Nhat Khanh")
        cy.get("[name='email']").type("khanh@gmail.com")
@@ -14,8 +20,19 @@ describe("Test Contact US for via WebdriverUni", () => {
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should("have.property","charset").and('eq','UTF-8')
+
+        //check title name of tab
+        cy.title().should('include','Contact Us')
+       
+        //check url was correct
+        cy.url().should('include','contactus')
+
+        cy.title().should('include','WebDriver')
         cy.get("[name='first_name']").type("Pham Phan")
         cy.get("[value='SUBMIT']").click()
+        cy.get("body").contains("Error: all fields are required")
+        cy.get("body").contains("Error: Invalid email address")
         
     })
 })
